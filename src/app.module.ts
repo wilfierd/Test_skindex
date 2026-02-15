cd87dc989f602d0f5ce46eb98c21f15c5ad0d204
wilfierd <vexbravor@gmail.com>
2026-02-15 18:40:43 +0700

    refactor: rm project boilerplate and unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -9,7 +7,5 @@ import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [PrismaModule, AuthModule, UsersModule, PostsModule, CommentsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
